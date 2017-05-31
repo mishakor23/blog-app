f22e7a28a2a128b960767c7d4c186f005bd0aa8c
Mikhail Korotkikh <mishakor23@gmail.com>
2017-05-31 15:27:43 +0300

    create posts_index component and define root route

---
+++ src/components/posts_index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PostsIndex extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Post Index
+      </div>
+    );
+  }
+
+}
+
+export default PostsIndex;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+import PostsIndex from './components/posts_index'
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+    document.getElementById('root'));
 registerServiceWorker();
