bee125db8a32f6379ea400342f13f7bc113c9e45
Mikhail Korotkikh <mishakor23@gmail.com>
2017-05-31 16:35:48 +0300

    fixed spelling mistakes and define Posts reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-export conts FETCH_POSTS = fetch_posts;
+export const FETCH_POSTS = 'fetch_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
-constr API_KEY = '?key=qwertyasdf'
+const API_KEY = '?key=qwertyasdf';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
