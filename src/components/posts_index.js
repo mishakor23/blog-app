ee111b302604d566787607deb8e019079d62ad25
Mikhail Korotkikh <mishakor23@gmail.com>
2017-05-31 17:28:43 +0300

    fetch posts and display them on the page

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,15 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import _ from 'lodash';
+import { fetchPosts } from '../actions';
 
 class PostsIndex extends React.Component {
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    })
+  }
 
   render() {
     return (
-      <div>
-        Post Index
+      <div className="container">
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     );
   }
 
 }
 
-export default PostsIndex;
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
