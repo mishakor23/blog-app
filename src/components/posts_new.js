a92af402bc427af8a62e3cbf4afd26bc8a8e08de
Mikhail Korotkikh <mishakor23@gmail.com>
2017-06-01 12:15:19 +0300

    add from to create new post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,47 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends React.Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label htmlFor="">{field.label}</label>
+        <input className="form-control"
+          type="text"
+          {...field.input}
+        />
+      </div>
+    );
+  }
+
+  renderTagsField() {
+
+  }
 
   render() {
     return (
-      <div>Posts new</div>
+      <form>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Tags"
+          name="tags"
+          component={this.renderField}
+        />
+        <Field
+          label="Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
     );
   }
 
 }
 
-export default PostsNew;
+export default reduxForm({
+  from: 'PostsNewForm'
+})(PostsNew);
