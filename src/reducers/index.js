5787cca614239c107f708bf28f12d980c4cfd4cf
Mikhail Korotkikh <mishakor23@gmail.com>
2017-06-01 11:43:55 +0300

    add redux-form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer} from 'redux-form';
+
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  from: formReducer
 });
 
 export default rootReducer;
