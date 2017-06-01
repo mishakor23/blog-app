56e7ef9c183cbd93ad82d9b757702d022e524eed
Mikhail Korotkikh <mishakor23@gmail.com>
2017-06-01 13:02:26 +0300

    fixed spelling errors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,7 @@ import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
-  from: formReducer
+  form: formReducer
 });
 
 export default rootReducer;
